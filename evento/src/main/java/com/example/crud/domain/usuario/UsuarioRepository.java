32ac9226eee5abf74ccf35d1451f197bcb9ec6e1
cassianohidekiumc <11212101074@alunos.umc.br>
2025-03-26 11:18:39 -0300

    Delete evento directory

--- evento/src/main/java/com/example/crud/domain/usuario/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.domain.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {}
--- evento/target/classes/db/migration/V1__create-table-usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuario (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nome TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL
-);
